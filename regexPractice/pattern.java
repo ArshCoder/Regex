b329f0ae2e93670f7cf3fd10acfb75a5513aa4a0
CoderArsh <arshjeetb08@gmail.com>
2021-10-26 15:49:03 +0530

    Added: To check no special character at beginning

--- regexPractice/pattern.java
+++ regexPractice/pattern.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class pattern {
     public static boolean isValidPinCode(String pinCode) {
-        String regex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
+        String regex = "^[a-z A-Z]{0}[1-9]{1}[0-9]{2}[0-9]{3}$";
         Pattern pattern = Pattern.compile(regex);
         if (pinCode == null) {
             return false;
@@ -18,5 +18,8 @@ public class pattern {
     public static void main(String[] args) {
         String pin = "400088";
         System.out.println(pin + ":" + isValidPinCode(pin));
+
+        String pin2 = "A400088";
+        System.out.println(pin2 + ":" + isValidPinCode(pin2));
     }
 }
