988493e7502865570fde327fecc618c4bff02ffa
CoderArsh <arshjeetb08@gmail.com>
2021-10-26 15:53:30 +0530

    Added: To check no special character at the end of pin

--- regexPractice/pattern.java
+++ regexPractice/pattern.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class pattern {
     public static boolean isValidPinCode(String pinCode) {
-        String regex = "^[a-z A-Z]{0}[1-9]{1}[0-9]{2}[0-9]{3}$";
+        String regex = "^[a-z A-Z]{0}[1-9]{1}[0-9]{2}[0-9]{3}[a-z A-Z]{0}$";
         Pattern pattern = Pattern.compile(regex);
         if (pinCode == null) {
             return false;
@@ -21,5 +21,8 @@ public class pattern {
 
         String pin2 = "A400088";
         System.out.println(pin2 + ":" + isValidPinCode(pin2));
+
+        String pin3 = "400088B";
+        System.out.println(pin3 + ":" + isValidPinCode(pin3));
     }
 }
