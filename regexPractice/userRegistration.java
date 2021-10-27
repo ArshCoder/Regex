7f25bab0caca1d7b670ad7bd29e2830a4948d137
CoderArsh <arshjeetb08@gmail.com>
2021-10-27 13:30:24 +0530

    Added: To check valid email address of user

--- regexPractice/userRegistration.java
+++ regexPractice/userRegistration.java
@@ -5,8 +5,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class userRegistration {
-    public static boolean isValidUser(String userName){
-        String regex = "^[A-Z]{1}[a-z A-Z]{2,}+$";
+    public static boolean isValidUser(String userName) {
+        String regex = "^[A-Z]{1}[a-z A-Z]{2,}$";
+
         Pattern pattern = Pattern.compile(regex);
         if (userName == null) {
             return false;
@@ -14,6 +15,18 @@ public class userRegistration {
         Matcher match = pattern.matcher(userName);
         return match.matches();
     }
+
+    public static boolean isValidEmail(String email) {
+        String regex = "^[a-z.]{2,}[a-z]{2,}+@(bl.co.)[a-z]{2,3}$";
+
+        Pattern pattern = Pattern.compile(regex);
+        if (email == null) {
+            return false;
+        }
+        Matcher match = pattern.matcher(email);
+        return match.matches();
+    }
+
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -24,5 +37,11 @@ public class userRegistration {
         System.out.println("Enter a LastName Start with capital letter and at least 3 character: ");
         String lastName = scan.nextLine();
         System.out.println(lastName + ":" + isValidUser(lastName));
+
+        System.out.println("Enter a Valid email : ");
+        String email = scan.nextLine();
+        System.out.println(email + " : " + isValidEmail(email));
+
+
     }
 }
